aa8d25205e40d31393644ae4e8d1495c8a213bb8
Zac Messinger <zdmessinger@gmail.com>
2024-08-18 12:25:59 -0700

    updated wedding schedule

--- src/routes/Wedding.js
+++ src/routes/Wedding.js
@@ -1,6 +1,14 @@
 import React from "react";
 import RouteWrapper from "../components/RouteWrapper";
 
+const events = [
+	{ time: "3:30PM - 4:00PM", title: "Guest Arrival", location: "Kamani Cove (1 Hotel)" },
+	{ time: "4:00PM - 4:30PM", title: "Wedding Ceremony", location: "Kamani Cove (1 Hotel)" },
+	{ time: "4:30PM - 5:45PM", title: "Cocktail Hour", location: "Kamani Cove (1 Hotel)" },
+	{ time: "6:00PM - 8:00PM", title: "Reception & Dinner", location: "Hanalei Room (1 Hotel)" },
+	{ time: "8:00PM - 12:00AM", title: "Late Night Reception", location: "Kukui Room (1 Hotel)" },
+];
+
 function Wedding() {
 	return (
 		<RouteWrapper backgroundColor="bg-white">
@@ -25,22 +33,48 @@ function Wedding() {
 						</p>
 					</div>
 					<div className="text-left mt-10">
-						<h3 className="text-black text-xl font-roboto pb-5 text-left leading-6">Thursday, September 19th, 2024 (Wedding Day)</h3>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Guest Arrival</p>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Kamani Cove (1 Hotel)</p>
-						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">3:30PM - 4:00PM</p>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Wedding Ceremony</p>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Kamani Cove (1 Hotel)</p>
-						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">4:00PM - 4:30PM </p>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Cocktail Hour</p>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Kamani Cove (1 Hotel)</p>
-						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">4:30PM - 5:45PM</p>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Wedding Reception & Dinner</p>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Hanalei Room (1 Hotel)</p>
-						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">6:00PM - 8:00PM</p>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Late Night Reception</p>
-						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Kukui Room (1 Hotel)</p>
-						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">8:00PM - 12:00AM</p>
+						<h3 className="text-black text-xl font-roboto text-left leading-6">Thursday, September 19th, 2024 (Wedding Day)</h3>
+						<div className="">
+							<div className="mt-4 flow-root">
+								<div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+									<div className="inline-block min-w-full py-2 align-middle">
+										<table className="min-w-full divide-y divide-gray-300">
+											<thead>
+												<tr>
+													<th
+														scope="col"
+														className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8"
+													>
+														Time
+													</th>
+													<th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+														Title
+													</th>
+													<th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+														Location
+													</th>
+												</tr>
+											</thead>
+											<tbody className="divide-y divide-gray-200 bg-white">
+												{events.map((e) => (
+													<tr key={e.title}>
+														<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
+															{e.time}
+														</td>
+														<td className="whitespace-nowrap px-3 py-4 text-md leading-6 text-gray-600 font-italiana font-extrabold">
+															{e.title}
+														</td>
+														<td className="whitespace-nowrap px-3 py-4 text-md leading-6 text-gray-600 font-italiana font-extrabold">
+															{e.location}
+														</td>
+													</tr>
+												))}
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 					<div className="text-left mt-10">
 						<h3 className="text-black text-xl font-roboto pb-5 text-left leading-6">Friday, September 20th, 2024 (Day After)</h3>
