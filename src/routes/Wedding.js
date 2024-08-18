04fcf3c824ec9511ee40c1c3a80b7b4f5ebf86d0
Zac Messinger <zdmessinger@gmail.com>
2024-08-17 22:20:20 -0700

    updated wedding schedule

--- src/routes/Wedding.js
+++ src/routes/Wedding.js
@@ -23,13 +23,24 @@ function Wedding() {
 							We are currently looking into providing group transportation given that the port is on the south shore and will update
 							once confirmed. Please plan to be ready for departure from the 1Hotel by 12:15PM.
 						</p>
-						<p className="text-md leading-6 text-gray-600 font-roboto">
-							Reach out to Zac and Lauren directly if you are interested in joining us!
-						</p>
 					</div>
 					<div className="text-left mt-10">
 						<h3 className="text-black text-xl font-roboto pb-5 text-left leading-6">Thursday, September 19th, 2024 (Wedding Day)</h3>
-						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">Itinerary to come!</p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Guest Arrival</p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Kamani Cove (1 Hotel)</p>
+						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">3:30PM - 4:00PM</p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Wedding Ceremony</p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Kamani Cove (1 Hotel)</p>
+						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">4:00PM - 4:30PM </p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Cocktail Hour</p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Kamani Cove (1 Hotel)</p>
+						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">4:30PM - 5:45PM</p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Wedding Reception & Dinner</p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Hanalei Room (1 Hotel)</p>
+						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">6:00PM - 8:00PM</p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Late Night Reception</p>
+						<p className="text-lg leading-6 text-gray-600 font-italiana font-extrabold">Kukui Room (1 Hotel)</p>
+						<p className="text-md leading-6 text-gray-600 font-roboto pb-5">8:00PM - 12:00AM</p>
 					</div>
 					<div className="text-left mt-10">
 						<h3 className="text-black text-xl font-roboto pb-5 text-left leading-6">Friday, September 20th, 2024 (Day After)</h3>
